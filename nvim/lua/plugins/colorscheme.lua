54c9f0c7bc86770f56a34a5d84fac40ead86c6dc
Oleg Tverdyy <alextverdyy@gmail.com>
2025-06-30 20:18:00 +0200

    Setup basic config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,21 @@
-return { 
-	"catppuccin/nvim", 
-name = "catppuccin", 
-priority = 1000, 
-config = function()
-	vim.cmd([[colorscheme catppuccin-mocha]])
-end
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme catppuccin-mocha]])
+        end
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    theme = "catppuccin"
+                }
+            }
+        end
+    }
 }
